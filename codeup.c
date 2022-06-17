d56864cb0b2b4ad6af78e08ec3d7187353328276
9dahugh <9dahugh@gmail.com>
2022-06-17 14:40:44 +0900

    add all files

---
+++ codeup.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+
+    if (a == 1)
+        printf("1");
+    if (a == 2)
+        printf("2");
+    if (a == 3)
+        printf("3");
+    if (a == 4)
+        printf("4");
+    if (a == 5)
+        printf("5");
+    if (a == 6)
+        printf("6");
+}
